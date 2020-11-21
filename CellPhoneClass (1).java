7f0ae4a441225776df82ce87017ddae6be15972a
Ajayduvva <66180348+Ajayduvva@users.noreply.github.com>
2020-11-21 03:32:47 -0500

    Ajay

---
+++ CellPhoneClass (1).java
@@ -0,0 +1,69 @@
+package com.example.myfinalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class CellPhoneClass extends AppCompatActivity {
+    DatabaseReference databaseReference;
+    RecyclerView recyclerView;
+    ArrayList<ProductClass> list;
+    ProductAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.productpage);
+        Button logo = findViewById(R.id.btn_logo2);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                startActivity(intent);
+            }
+        });
+
+        recyclerView = (RecyclerView) findViewById(R.id.myRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<ProductClass>();
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Products");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren()){
+                    ProductClass p = dataSnapshot1.getValue(ProductClass.class);
+                    if(p.getCategory().equals("CELL PHONES")){
+                        list.add(p);
+                    }
+                }
+                adapter = new ProductAdapter(CellPhoneClass.this,list);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(CellPhoneClass.this,"Database Error",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
